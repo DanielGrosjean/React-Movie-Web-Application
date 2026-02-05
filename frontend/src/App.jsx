171201b195f5fd8cd419d2a0f911115abeeff9b7
Daniel Grosjean <daniel.c.grosjean@gmail.com>
2026-02-05 13:11:33 -0700

    almost finished

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,18 +3,19 @@ import Favorites from './pages/Favorites'
 import Home from './pages/Home'
 import { Routes, Route } from 'react-router-dom'
 import NavBar from './components/NavBar'
+import { MovieProvider } from './contexts/MovieContext'
 
 function App() {
   return (
-    <div>
+    <MovieProvider>
       <NavBar />
-      <main className='main-content'>
+      <main className="main-content">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/favorites" element={<Favorites />} />
         </Routes>
       </main>
-    </div>
+    </MovieProvider>
   )
 }
 
