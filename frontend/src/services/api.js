0194a7e3f71cb7364aa8c334f059c10b0118e884
Daniel Grosjean <daniel.c.grosjean@gmail.com>
2026-02-05 12:18:24 -0700

    added api calls

---
+++ frontend/src/services/api.js
@@ -0,0 +1,14 @@
+const API_KEY = "bce136dd18a9218e0cb6d8ed9de465ca"
+const BASE_URL = "https://api.themoviedb.org/3"
+
+export const getPopularMovies = async () => {
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`)
+    const data = await response.json()
+    return data.results
+}
+
+export const searchMovies = async (query) => {
+    const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`)
+    const data = await response.json()
+    return data.results
+}
