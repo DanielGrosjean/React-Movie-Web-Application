2e1ac3cbedde55f8aea1fa7624da270b972f684d
Daniel Grosjean <daniel.c.grosjean@gmail.com>
2026-02-05 08:20:07 -0700

    added pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,17 @@
 import './App.css'
+import Favorites from './pages/Favorites'
+import Home from './pages/Home'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
-  const movieNumber = 2;
-
   return (
-    <>
-    
-    </>
+    <main className='main-content'>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/favorites" element={<Favorites />} />
+      </Routes>
+    </main>
   )
 }
-
 
 export default App
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,42 @@
+import MovieCard from "../components/MovieCard"
+import {useState} from "react"
+
+
+function Home () {
+
+    const [searchQuery, setSearchQuery] = useState("")
+
+    const movies = [
+        {id: 1, title: "F1", release_date: "2025"},
+        {id: 2, title: "Twisters", release_date: "2025"},
+        {id: 3, title: "Running Man", release_date: "2026"},
+    ]
+
+    const handleSearch = (e) => {
+        e.preventDefault()
+        alert(searchQuery)
+    }
+
+    return (
+        <div className="home">
+            <form onSubmit={handleSearch} className="search-form">
+                <input 
+                type="text" 
+                placeholder="Search for movies..." 
+                className="search-input" 
+                value={searchQuery}
+                onChange={(e) => setSearchQuery(e.target.value)}
+                />
+                <button type="submit" className="search-button">Search</button>
+            </form>
+
+            <div className="movies-grid">
+                {movies.map((movie) => (
+                    <MovieCard movie={movie} key={movie.id} />
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default Home
